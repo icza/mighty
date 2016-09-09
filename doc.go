f4b3a00f851d3e979def0238a93670cc48bba5ba
Andras Belicza <iczaaa@gmail.com>
2016-09-09 14:16:25 +0200

    Doc formatting

--- doc.go
+++ doc.go
@@ -24,7 +24,7 @@ But the recommended way is to acquire method values returned by functions of mig
 	// Expect the read byte to be 'a' AND returned error to be nil
 	expEq(byte('a'))(bytes.NewBuffer([]byte{'a'}).ReadByte())
 
-Example #1: testing math.Abs()
+Example 1 Testing math.Abs()
 
 Without mighty it could look like this:
 
@@ -43,7 +43,7 @@ Using mighty:
 		eq(c.exp, math.Abs(c.in))
 	}
 
-Example #2: testing reading from bytes.Buffer
+Example 2 Testing reading from bytes.Buffer
 
 Without mighty it could look like this:
 
@@ -69,5 +69,5 @@ Using mighty:
 	expEq(4)(r.Read(p))
 	eq("data", string(p))
 
- */
+*/
 package mighty
