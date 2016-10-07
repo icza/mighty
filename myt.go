f73d7a6cc23a45d28ee364e83191ef675a9fa1db
Andras Belicza <iczaaa@gmail.com>
2016-10-07 11:16:21 +0200

    Added new methods Myt.Near() and Myt.ExpNear(). Added utility functions Near() and ExpNear(). The near logic is exported: NearLogic(). Some refactoring.

--- myt.go
+++ myt.go
@@ -25,36 +25,41 @@ func (m Myt) Neq(v1, v2 interface{}, errs ...error) {
 	m.ExpNeq(v1)(v2, errs...)
 }
 
+// Near reports an error if the float64 exp is not "near" to got,
+// or an optional non-nil error is provided.
+// "near" is defined by the NearLogic() function.
+func (m Myt) Near(exp, got, eps float64, errs ...error) {
+	m.ExpNear(exp, eps)(got, errs...)
+}
+
 // ExpEq takes the expected value and returns a function which
 // only takes the 'got' value and an optional error.
 //
 // The following multiline code:
-//     exp := some_expected_value
 //     got, err := SomeFunc()
-//     Eq(exp, got, err)
+//     Eq(someExpectedValue, got, err)
 //
 // Is equivalent to this single line:
-//     ExpEq(exp)(SomeFunc())
+//     ExpEq(someExpectedValue)(SomeFunc())
 func (m Myt) ExpEq(exp interface{}) func(got interface{}, errs ...error) {
 	return func(got interface{}, errs ...error) {
-		var err error
-		if len(errs) > 0 && errs[0] != nil {
-			err = errs[0]
+		err := getErr(errs...)
+		if exp == got && err == nil {
+			return
 		}
-		if exp != got || err != nil {
-			function, line := getFuncLine()
-			if err == nil {
-				m.Errorf("[%s:%d] Expected: %v, got: %v", function, line, exp, got)
-			} else {
-				m.Errorf("[%s:%d] Expected: %v, got: %v, error: %v", function, line, exp, got, err)
-			}
-			// Common mistake is to provide constants as exp whose default value will be applied
-			// when packed into interface{} which might not be the case in case of direct comparison.
-			// Provide warning for such likely cause.
-			if exp != got && exp != nil && got != nil {
-				if texp, tgot := reflect.TypeOf(exp), reflect.TypeOf(got); texp != tgot {
-					m.Errorf("\tType of expected and got does not match! exp type: %v, got type: %v", texp, tgot)
-				}
+
+		function, line := getFuncLine()
+		if err == nil {
+			m.Errorf("[%s:%d] Expected: %v, got: %v", function, line, exp, got)
+		} else {
+			m.Errorf("[%s:%d] Expected: %v, got: %v, error: %v", function, line, exp, got, err)
+		}
+		// Common mistake is to provide constants as exp whose default value will be applied
+		// when packed into interface{} which might not be the case in case of direct comparison.
+		// Provide warning for such likely cause.
+		if exp != got && exp != nil && got != nil {
+			if texp, tgot := reflect.TypeOf(exp), reflect.TypeOf(got); texp != tgot {
+				m.Errorf("\tType of expected and got does not match! exp type: %v, got type: %v", texp, tgot)
 			}
 		}
 	}
@@ -64,27 +69,84 @@ func (m Myt) ExpEq(exp interface{}) func(got interface{}, errs ...error) {
 // takes only the 2nd value and an optional error.
 //
 // The following multiline code:
-//     v1 := some_value1
 //     v2, err := SomeFunc()
-//     Neq(v1, v2, err)
+//     Neq(someValue1, v2, err)
 //
 // Is equivalent to this single line:
-//     ExpNeq(v1)(SomeFunc())
+//     ExpNeq(someValue1)(SomeFunc())
 func (m Myt) ExpNeq(v1 interface{}) func(v2 interface{}, errs ...error) {
 	return func(v2 interface{}, errs ...error) {
-		var err error
-		if len(errs) > 0 && errs[0] != nil {
-			err = errs[0]
+		err := getErr(errs...)
+		if v1 != v2 && err == nil {
+			return
 		}
-		if v1 == v2 || err != nil {
-			function, line := getFuncLine()
-			if err == nil {
-				m.Errorf("[%s:%d] Expected mismatch: %v, got: %v", function, line, v1, v2)
-			} else {
-				m.Errorf("[%s:%d] Expected mismatch: %v, got: %v, error: %v", function, line, v1, v2, err)
-			}
+
+		function, line := getFuncLine()
+		if err == nil {
+			m.Errorf("[%s:%d] Expected mismatch: %v, got: %v", function, line, v1, v2)
+		} else {
+			m.Errorf("[%s:%d] Expected mismatch: %v, got: %v, error: %v", function, line, v1, v2, err)
 		}
 	}
+}
+
+// ExpNear takes the expected and epslion values and returns a function which
+// only takes the 'got' value and an optional error.
+//
+// The following multiline code:
+//     got, err := SomeFunc()
+//     Near(someExpectedValue, got, someEpsilon, err)
+//
+// Is equivalent to this single line:
+//     ExpNear(someExpectedValue, someEpsilon)(SomeFunc())
+func (m Myt) ExpNear(exp, eps float64) func(got float64, errs ...error) {
+	return func(got float64, errs ...error) {
+		err := getErr(errs...)
+		if err == nil && NearLogic(exp, got, eps) {
+			return
+		}
+
+		function, line := getFuncLine()
+		if err == nil {
+			m.Errorf("[%s:%d] Expected: %v, got: %v, with eps: %v)", function, line, exp, got, eps)
+		} else {
+			m.Errorf("[%s:%d] Expected: %v, got: %v, with eps: %v, error: %v", function, line, exp, got, eps, err)
+		}
+	}
+}
+
+// NearLogic checks if 2 float64 numbers are "near" to each other.
+// The caller is responsible to provide a sensible epsilon.
+// "near" is defined as the following:
+//     near := Math.Abs(a, b) < eps
+//
+// Corner cases:
+//  1. if a==b, result is true (eps will not be checked, may be NaN)
+//  2. Inf is near to Inf (even if eps=NaN; consequence of 1.)
+//  3. -Inf is near to -Inf (even if eps=NaN; consequence of 1.)
+//  4. NaN is not near to anything (not even to NaN)
+//  5. eps=Inf results in true (unless any of a or b is NaN)
+func NearLogic(a, b, eps float64) bool {
+	// Quick check, also handles infinities:
+	if a == b {
+		return true
+	}
+
+	diff := a - b
+	if diff < 0 {
+		diff = -diff
+	}
+
+	return diff < eps
+}
+
+// getErr is a utility function which returns the optional error
+// from the variadic errors.
+func getErr(errs ...error) error {
+	if len(errs) > 0 && errs[0] != nil {
+		return errs[0]
+	}
+	return nil
 }
 
 // getFuncLine reports the function name and line number of the first caller
@@ -110,38 +172,58 @@ type Func2ArgsErr func(interface{}, interface{}, ...error)
 // and returns a function which takes 1 interface{} argument and an optional error (in the form of a variadic parameter).
 type Func1ArgFunc1ArgErr func(interface{}) func(interface{}, ...error)
 
-// Eq returns a method value of Myt{t}.Eq.
+// Func3FloatsErr is a type describing a function which takes 3 float64 arguments
+// and an optional error (in the form of a variadic parameter).
+type Func3FloatsErr func(float64, float64, float64, ...error)
+
+// Func2FloatsFunc1FloatErr is a type describing a function which takes 2 float64 arguments
+// and returns a function which takes 1 float64 argument and an optional error (in the form of a variadic parameter).
+type Func2FloatsFunc1FloatErr func(float64, float64) func(float64, ...error)
+
+// Eq returns a method value of Myt{tb}.Eq.
 // tb may be a *testing.T or *testing.B value.
 func Eq(tb testing.TB) Func2ArgsErr {
 	return Myt{tb}.Eq
 }
 
-// Neq returns a method value of Myt{t}.Neq.
+// Neq returns a method value of Myt{tb}.Neq.
 // tb may be a *testing.T or *testing.B value.
 func Neq(tb testing.TB) Func2ArgsErr {
 	return Myt{tb}.Neq
 }
 
-// EqNeq returns 2 method values: Myt{t}.Eq and Myt{t}.Neq.
+// Near returns a method value of Myt{tb}.Near.
+// tb may be a *testing.T or *testing.B value.
+func Near(tb testing.TB) Func3FloatsErr {
+	return Myt{tb}.Near
+}
+
+// EqNeq returns 2 method values: Myt{tb}.Eq and Myt{tb}.Neq.
 // tb may be a *testing.T or *testing.B value.
 func EqNeq(tb testing.TB) (Func2ArgsErr, Func2ArgsErr) {
 	myt := Myt{tb}
 	return myt.Eq, myt.Neq
 }
 
-// ExpEq returns a method value of Myt{t}.ExpEq.
+// ExpEq returns a method value of Myt{tb}.ExpEq.
 // tb may be a *testing.T or *testing.B value.
 func ExpEq(tb testing.TB) Func1ArgFunc1ArgErr {
 	return Myt{tb}.ExpEq
 }
 
-// ExpNeq returns a method value of Myt{t}.ExpNeq.
+// ExpNeq returns a method value of Myt{tb}.ExpNeq.
 // tb may be a *testing.T or *testing.B value.
 func ExpNeq(tb testing.TB) Func1ArgFunc1ArgErr {
 	return Myt{tb}.ExpNeq
 }
 
-// EqExpEq returns 2 method values: Myt{t}.Eq and Myt{t}.ExpEq.
+// ExpNear returns a method value of Myt{tb}.ExpNear.
+// tb may be a *testing.T or *testing.B value.
+func ExpNear(tb testing.TB) Func2FloatsFunc1FloatErr {
+	return Myt{tb}.ExpNear
+}
+
+// EqExpEq returns 2 method values: Myt{tb}.Eq and Myt{tb}.ExpEq.
 // tb may be a *testing.T or *testing.B value.
 func EqExpEq(tb testing.TB) (Func2ArgsErr, Func1ArgFunc1ArgErr) {
 	myt := Myt{tb}
--- myt_test.go
+++ myt_test.go
@@ -2,6 +2,7 @@ package mighty
 
 import (
 	"errors"
+	"math"
 	"testing"
 )
 
@@ -47,11 +48,45 @@ func TestMytEqNeq(t *testing.T) {
 	}
 }
 
+func TestMytNear(t *testing.T) {
+	tb := &TBMock{}
+	myt := Myt{tb}
+
+	errs := []error{errors.New("test error")}
+	cases := []struct {
+		exp, got, eps float64
+		errs          []error
+		errCalls      int
+	}{
+		{1.0, 1.0, 1e-6, nil, 0},
+		{1.0, 1.0, 1e-6, errs, 1},
+		{1.0, 1.001, 1e-2, nil, 0},
+		{1.0, 1.001, 1e-2, errs, 1},
+		{1.0, 1.001, 1e-4, nil, 1},
+		{1.0, 1.001, 1e-4, errs, 1},
+	}
+
+	for i, c := range cases {
+		exp, got := c.exp, c.got
+		for j := 0; j < 2; j++ { // 2 cycles to switch got end exp
+			if j > 0 {
+				exp, got = got, exp
+			}
+			tb.errCalls = 0
+			myt.Near(c.exp, c.got, c.eps, c.errs...)
+			if c.errCalls != tb.errCalls {
+				t.Errorf("[i=%d] Expected: %d, got: %d", i, c.errCalls, tb.errCalls)
+			}
+		}
+	}
+}
+
 func TestFuncs(t *testing.T) {
 	tb := &TBMock{}
 
 	Eq(tb)(1, 2)
 	Neq(tb)(1, 2)
+	Near(tb)(1, 1, 1e-6)
 
 	eq, neq := EqNeq(tb)
 	eq(1, 2)
@@ -59,10 +94,51 @@ func TestFuncs(t *testing.T) {
 
 	ExpEq(tb)(1)(2, nil)
 	ExpNeq(tb)(1)(2, nil)
+	ExpNear(tb)(1, 1e-6)(1, nil)
 
 	eq, expEq := EqExpEq(tb)
 	eq(1, 2)
 	expEq(1)(2, nil)
+}
+
+func TestNearLogic(t *testing.T) {
+	inf, neginf, nan := math.Inf(1), math.Inf(-1), math.NaN()
+	cases := []struct {
+		a, b, eps float64
+		exp       bool
+	}{
+		{1.0, 1.0, 1e-6, true},
+		{1.0, 1.001, 1e-6, false},
+		{1.0, 1.001, 1e-2, true},
+
+		// Corner cases
+		{inf, 1.001, 1e-2, false},
+		{neginf, 1.001, 1e-2, false},
+		{inf, inf, 1e-2, true},
+		{neginf, neginf, 1e-2, true},
+		{inf, neginf, 1e-2, false},
+
+		{1.0, 1.1, inf, true},
+		{1.0, inf, inf, false},
+		{inf, inf, inf, true},
+		{neginf, neginf, inf, true},
+
+		{1.0, nan, 1e10, false},
+		{1.0, nan, inf, false},
+		{nan, nan, 1e10, false},
+		{nan, nan, inf, false},
+
+		{1.0, 1.0, nan, true},
+		{1.0, 1.001, nan, false},
+		{inf, inf, nan, true},
+		{neginf, neginf, nan, true},
+	}
+
+	for i, c := range cases {
+		if got := NearLogic(c.a, c.b, c.eps); c.exp != got {
+			t.Errorf("[i=%d] Expected: %v, got: %v", i, c.exp, got)
+		}
+	}
 }
 
 func TestGetFileLineUnknown(t *testing.T) {
